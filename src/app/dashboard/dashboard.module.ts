01e9bfeb0d98a7da11d3d996ee697ee92a12adbe
[anamikoye] <[anamikoye52@gmail.com]>
2017-11-04 22:19:29 +0300

    lol

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { ChartsModule } from 'ng2-charts/ng2-charts';
 
+import { CommonModule } from '@angular/common';
+
 
 import { CalendarModule } from 'angular-calendar';
 // import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -12,6 +14,7 @@ import { DashboardRoutingModule } from './dashboard-routing.module';
 @NgModule({
   imports: [
     DashboardRoutingModule,
+    CommonModule,
     ChartsModule,
     // BrowserAnimationsModule,
     CalendarModule.forRoot()
--- src/app/pages/sample-event/sample-event.component.ts
+++ src/app/pages/sample-event/sample-event.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-sample-event',
@@ -6,6 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sample-event.component.scss']
 })
 export class SampleEventComponent implements OnInit {
+
+  p = 1;
+  categories = ['Information Technology', 'Financial Economics', 'Commerce', 'Law', 'Information Technology', 'Financial Economics', 'Commerce', 'Law', 'Information Technology', 'Financial Economics', 'Commerce', 'Law'];
 
   constructor() { }
 
